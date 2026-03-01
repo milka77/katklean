ed2d5b987c3aa86db4234e617a1d9e18b23bda8e
Milka77 <krisztian.keseru@gmail.com>
2026-03-01 13:59:15 +0000

    Updated booking ref logik

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -56,7 +56,7 @@ class Booking extends Model
             $latest = Booking::latest('id')->first();
             $number = $latest ? $latest->id + 1 : 1;
 
-            $booking->reference = 'BK-' . str_pad($number, 6, '0', STR_PAD_LEFT);
+            $booking->reference = 'KTK-' . now()->format('y') . '-' . str_pad($number, 6, '0', STR_PAD_LEFT);
         });
     }
 
