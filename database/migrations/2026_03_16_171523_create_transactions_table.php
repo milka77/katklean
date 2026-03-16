f2198a612e2a9b0c837d50e7fb060cba8f5bc848
Milka77 <krisztian.keseru@gmail.com>
2026-03-16 20:00:52 +0000

    created Transaction create and store methods with validation and view with routes

--- database/migrations/2026_03_16_171523_create_transactions_table.php
+++ database/migrations/2026_03_16_171523_create_transactions_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->string('type');
+            $table->decimal('amount', 10, 2);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('description')->nullable();
+            $table->string('booking_reference')->nullable();
+            $table->string('address')->nullable();
+            $table->string('service')->nullable();
+            $table->string('distance')->nullable();
+            $table->string('exp_shop')->nullable();
+            $table->string('exp_product')->nullable();
             $table->timestamps();
         });
     }
