4a485cca31168769e10781107bcd513cdc72ab3b
Milka77 <krisztian.keseru@gmail.com>
2026-03-17 12:37:54 +0000

    Added extra title option to layout.blade.php to support dynamic page titles.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,14 +12,14 @@
             gtag('config', 'G-HKGPEN7RE0');
         </script>
     @endif
-    
+
     {{-- Laravel Cookie consent  --}}
     {!! CookieConsent::styles() !!}
     {{-- Laravel Cookie consent  --}}
 
     {{-- SEO & Social meta tags --}}
     <meta name="description" content="Reliable local cleaning company in Preston offering professional cleaning services for homes and properties.Local Cleaning Company in Preston | KatKlean">
-    <meta name="keywords" content="cleaning services preston, cleaners preston, domestic cleaning preston, commercial cleaning preston, house cleaning preston, end of tenancy cleaning preston, office cleaning preston, 
+    <meta name="keywords" content="cleaning services preston, cleaners preston, domestic cleaning preston, commercial cleaning preston, house cleaning preston, end of tenancy cleaning preston, office cleaning preston,
         deep cleaning preston, local cleaners preston lancashire">
 
 
@@ -41,15 +41,16 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     @vite('resources/css/app.css')
-    <title>{{ config('app.name', 'KatKlean') }}</title>
+    <title>@yield('title') {{ config('app.name', 'KatKlean') }}</title>
+
     @yield('extra-style')
     <style>
         body {
-        
+
         background-image: url('{{ asset('images/4545.png') }}');
         background-size: cover;
         background-position: fixed;
-        
+
         }
     </style>
 </head>
