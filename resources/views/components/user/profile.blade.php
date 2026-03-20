b888c122c9c0616e5f5f34a63080a233d676f6bc
Milka77 <krisztian.keseru@gmail.com>
2026-03-20 10:10:57 +0000

    Added upcomming booking to user profile and updated layout structure with link to bookings list

--- resources/views/components/user/profile.blade.php
+++ resources/views/components/user/profile.blade.php
@@ -5,7 +5,7 @@
 
       <div class="border w-full md:w-125 mx-auto rounded-2xl border-slate-300 bg-slate-100 p-2 md:p-5">
         <p class="text-center  pb-3">Welcome {{ Auth()->user()->first_name }} </p>
-        
+
         <div class="border rounded-2xl border-slate-300 bg-stone-50 pt-3">
 
             <div class="p-4 ">
@@ -18,9 +18,9 @@
                 <p>Last name: </p><p>{{ Auth()->user()->last_name }}</p>
               </div> --}}
               <div class="flex flex-row justify-between px-2 py1 mb-4">
-                  <p>Email: </p><p>{{ Auth()->user()->email }}</p>  
+                  <p>Email: </p><p>{{ Auth()->user()->email }}</p>
               </div>
-                
+
               <p class="px-2 text-lg font-semibold">Address</p>
               <hr class="text-slate-300 mb-2">
               <div class=" py1 ">
@@ -40,8 +40,8 @@
                       <form action="{{ route('address.destroy', $address) }}" method="POST">
                         @csrf
                         @method('delete')
-                        <button class="cursor-pointer">Delete</button>  
-                      </form>  
+                        <button class="cursor-pointer">Delete</button>
+                      </form>
                     </a>
                     <button class="bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded cursor-pointer"><a href="{{ route('address.edit', $address) }}">Update</a></button>
                   </div>
@@ -56,17 +56,55 @@
                     <button class="bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded cursor-pointer"><a href="{{ route('address.create') }}">Add</a></button>
                   </div>
               </div>
+
+                <p class="px-2 text-lg font-semibold">Bookings</p>
+                <hr class="text-slate-300 mb-2">
+                <div class=" py1 ">
+                    @if (Auth()->user()->bookings->first())
+                        @php
+                        $nextBooking = Auth()->user()->bookings
+                            ->where('start_at', '>=', now())
+                            ->sortBy('start_at')
+                            ->first();
+                        @endphp
+                        <p class="px-2 font-semibold pb-2">Your next upcoming booking:</p>
+                        <ul class="flex flex-col px-2 pb-3">
+                            <div class="flex flex-row justify-between">
+                                <li>Booking date:</li>
+                                <li> {{ $nextBooking->booking_date }} </li>
+                            </div>
+                            <div class="flex flex-row justify-between">
+                                <li>Booking reference</li>
+                                <li> {{ $nextBooking->reference }}</li>
+                            </div>
+
+                        </ul>
+                    @else
+                        <p class="px-2">You have not made any bookings yet.</p>
+                        <hr class="text-slate-300 my-2">
+
+                    @endif
+                    <div class="flex flex-row justify-center gap-4">
+                        <button class="bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded cursor-pointer"><a href="{{ route('profile.bookings') }}">My bookings</a></button>
+
+                        <button class="bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded cursor-pointer"><a href="{{ route('booking') }}">Book now</a></button>
+
+                    </div>
+
+
+                </div>
+
               <hr class="text-slate-300 my-2">
               <div class="flex flex-row justify-between">
                  <p class="text-xs pl-2">Account created at:</p><p class="text-xs pr-2">{{ Auth()->user()->created_at->format('d/m/Y H:i') }}</p>
               </div>
-              
+
               <hr class="text-slate-300 my-2">
             </div>
-          
+
         </div>
-      
-      
+
+
       </div>
     </div>
   @endsection
