be684daf1e2a78000669f5bf9a957f0457266ffd
Milka77 <krisztian.keseru@gmail.com>
2026-04-15 21:29:11 +0100

    updated subheader text

--- resources/views/components/site/contact.blade.php
+++ resources/views/components/site/contact.blade.php
@@ -22,7 +22,7 @@
 
 
         <h1 class="text-5xl text-center font-semibold text-black pb-4">Get in touch with us</h1>
-        <p class="text-lg font-semibold text-center pb-10">Do you have any question?<br>Don't hesitate, send us a message or call us.<br><i class="fa-solid fa-mobile-screen"></i> 07456517365</p>    
+        <p class="text-lg font-semibold text-center pb-10">Need a property cleaned properly?<br>Get in touch to discuss your requirements or request a quote.<br><i class="fa-solid fa-mobile-screen"></i> 07456517365</p>    
         <div class="flex flex-col md:flex-row items-center gap-8 w-[350px] md:w-[700px]">
           <div class="w-full">
             <label class="text-black" for="name">How can we call you? <span class="text-red-500">*</span></label>
