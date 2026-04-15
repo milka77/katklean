61bebf0e73d02afeb3907f515034724f07ebaf0e
Milka77 <krisztian.keseru@gmail.com>
2026-04-15 21:25:59 +0100

    updated hero section text

--- resources/views/components/site/hero.blade.php
+++ resources/views/components/site/hero.blade.php
@@ -1,10 +1,15 @@
 <section id="hero" class="min-h-screen bt-50 text-center text-sm text-black max-md:px-2 bg-[url('https://images-ext-1.discordapp.net/external/STIdhamuqC59oX1eexDv6WAkXiHGLdC5E2nE3yDSkKs/%3Fq%3D80%26w%3D1332%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1616486338812-3dadae4b4ace?format=webp')] bg-cover bg-center">
-   <div class="flex flex-col pb-50 items-center backdrop-blur-xs mask-b-from-55% w-full h-screen pt-30">
-        <h1 class="dm-serif-display-hero text-[45px]/[56px] md:text-[5rem]/[110px] mt-9 pt-15 max-w-4xl">
-          Professional cleaning. One home at a time.
+   <div class="flex flex-col pb-50 items-center backdrop-blur-xs mask-b-from-70% w-full h-screen pt-30">
+        <h1 class="dm-serif-display-hero text-[45px]/[56px] md:text-[5rem]/[110px] mt-9 pt-15 ">
+          Professional cleaning for properties <br> that need to be ready
         </h1>
 
-        <p class="text-lg mt-3 max-w-xl">Serving Preston and surrounding areas.</p>
+        <p class="text-2xl font-semibold py-3">End of Tenancy • After Builders • Deep Cleaning</p>
+
+        <p class="text-lg mt-3 ">
+            Reliable, high-standard cleaning for landlords, builders and homeowners. <br>
+            Focused on delivering a clean that’s ready for handover or presentation.
+        </p>
 
         <div class="flex flex-col-1 md:flex-col-2 mt-5 gap-7 content-center justify-center pt-20">
 
